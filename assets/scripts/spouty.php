aec8945382a37ee07ec1a6e47c1bba26f8389063
alta189 <me@alta189.com>
2012-06-05 11:42:07 -0400

    Force using a new connection

--- assets/scripts/spouty.php
+++ assets/scripts/spouty.php
@@ -1,6 +1,21 @@
 <?php 
 function isSpoutyOnline() {
-		$ch = curl_init();
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'http://rest.spouty.org/status');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+	curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+	$f = curl_exec($ch);
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if ($httpcode == 200) {
+		return true;
+	}
+	return false;
+}
+
+function isSpoutyOnline() {
+	$ch = curl_init();
 	curl_setopt ( $ch, CURLOPT_URL, 'http://rest.spouty.org/status' );
 	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
 	curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
@@ -12,3 +27,5 @@ function isSpoutyOnline() {
 	}
 	return false;
 }
+
+?>
