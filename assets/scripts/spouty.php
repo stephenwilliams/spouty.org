3422e8a9e962aee25f7a366654e19d5b128393dd
alta189 <me@alta189.com>
2012-06-05 11:26:44 -0400

    use the status page

--- assets/scripts/spouty.php
+++ assets/scripts/spouty.php
@@ -1,7 +1,7 @@
 <?php 
 function isSpoutyOnline() {
 		$ch = curl_init();
-	curl_setopt ( $ch, CURLOPT_URL, 'http://rest.spouty.org/cyborg' );
+	curl_setopt ( $ch, CURLOPT_URL, 'http://rest.spouty.org/status' );
 	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
 	curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 	$f = curl_exec ( $ch );
