12d9f190d11bd222028fafafd81f715776cc6747
alta189 <me@alta189.com>
2012-06-17 19:06:46 -0400

    More factoid help

--- help.php
+++ help.php
@@ -86,17 +86,116 @@ body {
 
 		<div class="row">
 			<div class="span12">
-				<p>What are factoids? Simply put, factoids are dynamic commands that anyone can create or modify. The factoid system can be used in many ways. For example, it could be used for a simple fun command or be used for a FAQ. Factoids are probably the single most important feature of Spouty. They make her useful to the community and the Spout team. The factoid system is completely open, <strong>unless</strong> you don't follow these simple rules</p>
+				<p>What are factoids? Simply put, factoids are dynamic commands that anyone can create or modify. The factoid system can be used in many ways. For example, it could be used for a simple fun command or be used for a FAQ. Factoids are probably the single most important feature of Spouty. They make her useful to the community and the Spout team. The factoid system is completely open for anyone to create, modify, and use <strong>unless</strong> you don't follow these simple rules</p>
 				<ol>
 					<li>Any user that creates a factoid that violates the Spout IRC rules will have their factoid permissions removed</li>
 					<li>Any user that creates a factoid that a Bot Admin decides is inappropriate may lose their factoid permissions</li>
 					<li>Any user that creates factoids with the intent to use them to spam will have their factoid permissions removed</li>
 					<li>Any user that abuses the factoid system will have their factoid permissions removed</li>
-				</ol>
+				</ol><br />
 				<p>Triggering factoids is easy. There are two kinds of factoids, global and local. A global factoid can be used in any channel and is the default when creating or modifying a factoid. A local factoid can only be used in the channel that it was created in. Likewise, there are two factoid prefixes, <code>!</code> and <code>?</code>. <code>!</code> will look for global factoids first and if it cannot find one, it will search for a local factoid. <code>?</code> will look for a local factoid first and if it cannot find one, it will search for a global factoid.
 				</p>
-				<p>
+			</div>
+		</div><br />
+		<div class="row">
+			<div class="span6">
+				<h4>Handlers - What do they do? What ones are available?
+				</h4> <p>
+				Handlers are what Spouty used to decide how a factoid is handled. They can range simple to more complex. The default factoid is <code>reply</code>. The <code>reply</code>, <code>action</code>, and <code>notice</code> handlers have the following variables: <br />
+				<table class="table table-striped table-bordered table-condensed" width="100%">
+					<thead>
+						<tr>
+							<th>Variable</th>
+							<th>Description</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>%0%</td>
+							<td>Will print the first argument provided to the factoid</td>
+						</tr>
+						<tr>
+							<td>%0-%</td>
+							<td>Will print all the arguments from the first and onwards</td>
+						</tr>
+						<tr>
+							<td>%0-2%</td>
+							<td>Will print the arguments from the first to the third</td>
+						</tr>
+						<tr>
+							<td>%0-2%</td>
+							<td>Will print the arguments from the first to the third</td>
+						</tr>
+						<tr>
+							<td>%nick%</td>
+							<td>Will print the nick of the user that triggered the factoid</td>
+						</tr>
+						<tr>
+							<td>%channel%</td>
+							<td>Will print the channel that the factoid was triggered in</td>
+						</tr>
+						<tr>
+							<td>%channel%</td>
+							<td>Will print the channel that the factoid was triggered in</td>
+						</tr>
+						<tr>
+							<td>%readUrl("http://some.url/")%</td>
+							<td>Will print the first line of the url.</td>
+						</tr>
+						<tr>
+							<td>%readRandLine("http://some.url/")%</td>
+							<td>Will print a random line of the url.</td>
+						</tr>
+						<tr>
+							<td>%rn%</td>
+							<td>Allows for multilined factoids. <span class="label label-important">WARNING</span> Requires permissions!</td>
+						</tr>
+					</tbody>
+				</table>	
 				</p>
+			</div>
+			<div class="span6">
+				<table class="table table-striped table-bordered table-condensed" width="100%">
+					<thead>
+						<tr>
+							<th>Handler</th>
+							<th>Aliases</th>
+							<th>Description</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>reply</td>
+							<td>say</td>
+							<td>Replies to a factoid with a message. Either to a channel or private message depending on where the factoid was triggered.</td>
+						</tr>
+						<tr>
+							<td>action</td>
+							<td>act</td>
+							<td>Performs and action to either a channel or private message.</td>
+						</tr>
+						<tr>
+							<td>notice</td>
+							<td></td>
+							<td>Sends a notice to the user who triggered the factoid.</td>
+						</tr>
+						<tr>
+							<td>alias</td>
+							<td></td>
+							<td>Forwards the trigger event to another factoid.</td>
+						</tr>
+						<tr>
+							<td>command</td>
+							<td>cmd</td>
+							<td>Forwards the trigger event to one of Spouty's commands.</td>
+						</tr>
+						<tr>
+							<td>python</td>
+							<td>py</td>
+							<td>Runs the factoid's contents through a python interpreter.</td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</section>
